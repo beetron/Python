fabe12df446ea0a5b1a2dc4c542c6deba74b0788
Brian <85221688+beetron@users.noreply.github.com>
2023-10-14 21:10:34 +0900

    Add files via upload

---
+++ Delete-Expired-Folders/Delete-Expired-Folders.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import datetime
+
+# Get directory path from environment variable
+directory_path = os.getenv("bshare_PyPath")
+
+# Set the hours/condition for folder deletion
+check_hours = 1
+time_now = datetime.datetime.now()
+
+# os.walk to search directory tree
+for root, dirs, files in os.walk(directory_path):
+    for item in dirs:
+        item_path = os.path.join(root, item)
+        # Check creation time of folders
+        creation_time = datetime.datetime.fromtimestamp(os.path.getctime(item_path))
+        # Check how many hours passed since creation
+        age = (time_now - creation_time).total_seconds() / 60  # 3600 for an hour
+
+        if age >= check_hours:
+            try:
+                shutil.rmtree(item_path)
+                print(f"Directory: {item_path} deleted")
+            except OSError as e:
+                print(f"Error deleting folder: {item_path} - {e}")
